373b34f7dbeddc014a92173375b291aa61a5c07a
gkqkehs7 <77993709+gkqkehs7@users.noreply.github.com>
2023-05-11 16:56:32 +0900

    #14 Connect to client with websocket

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,13 @@ const redis = Redis.getInstance();
 const logger = WinstonLogger.getInstance();
 
 app.set('port', 4000);
+
+app.all('/*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
